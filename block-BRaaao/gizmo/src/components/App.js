f6dc369036bc5d638ce876fdd21b49cce9125a8a
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-23 12:17:04 +0530

    Assignment: Articles App

---
+++ block-BRaaao/gizmo/src/components/App.js
@@ -0,0 +1,17 @@
+import Articles from './Articles';
+import Footer from './Footer';
+import Header from './Header';
+import Hero from './Hero';
+
+function App(props) {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Articles />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ block-BRaaao/gizmo/src/components/Articles.js
@@ -0,0 +1,54 @@
+function Articles() {
+  return (
+    <>
+      <section className="articles pad-top">
+        <div className="container2 flex mar-left jus-bet flex-wrap">
+          <div className="col col-1">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+          <div className="col col-1">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+          <div className="col col-1">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+          <div className="col col-2">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+          <div className="col col-2">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+          <div className="col col-2">
+            <img
+              src="https://cdn.shortpixel.ai/spai/q_lossy+w_998+to_avif+ret_img/https://www.mindful.org/content/uploads/What-Happens-When-We-Reconnect-with-Nature.jpg"
+              alt=""
+            ></img>
+            <h1>sdlfjsdlkfs</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Articles;
