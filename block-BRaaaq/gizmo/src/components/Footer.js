119d160710e58158f84f67c84ffa02d444c072c7
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-23 16:52:19 +0530

    Assignment: PropTypes to Articles App

---
+++ block-BRaaaq/gizmo/src/components/Footer.js
@@ -0,0 +1,29 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container-2 flex jus-bet items-center">
+          <small>
+            &copy; Copyright <strong>Ssk</strong>. All Rights Reserved |
+            Designed by <span>SarangKulkarni</span>
+          </small>
+          <nav className="social">
+            <ul className="flex items-center jus-bet">
+              <li className="social">
+                <a href="/">F</a>
+              </li>
+              <li className="social">
+                <a href="/">T</a>
+              </li>
+              <li className="social">
+                <a href="/">I</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
